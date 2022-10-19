85fa3b854c8b00644630ee7e0e30c19751195a15
Luigi9411 <luigi.sciavicco@gmail.com>
2022-10-19 19:52:53 +0200

    add-result

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let name = (prompt('Qual è il tuo nome?'))
+let lastName = (prompt('Qual è il tuo cognome?'))
+let color = (prompt('Qual è il tuo colore preferito?'))
+document.getElementById('result').innerHTML = name+lastName+color+22;
